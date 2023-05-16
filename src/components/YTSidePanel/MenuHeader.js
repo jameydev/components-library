2efa5f6b844a13ff7fc9a6ecf1708a3391aad42f
jameydev <jybryce4@gmail.com>
2023-05-16 18:32:29 -0500

    added space in MenuHeader

--- src/components/YTSidePanel/MenuHeader.js
+++ src/components/YTSidePanel/MenuHeader.js
@@ -4,6 +4,7 @@ export default function MenuHeader() {
     return (
         <div class="menu-header">
             <HamburgerIcon />
+            &nbsp;
             <img className="logo" src="#" alt="YOUR LOGO/ICON" />
         </div>
     );
