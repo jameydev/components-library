4bbbb9c29689c190af642038c8dc7ee62d6aacf8
jameydev <jybryce4@gmail.com>
2023-05-16 08:50:51 -0500

    started work on list; putting it together

--- src/components/YTSidePanel/SideMenu.js
+++ src/components/YTSidePanel/SideMenu.js
@@ -1,9 +1,12 @@
 import MenuHeader from "./MenuHeader";
+import List from "./List";
+import listItems from "./items";
 
 export default function SideMenu() {
     return (
         <div className="side-menu-wrapper">
             <MenuHeader />
+            <List ordered={false} items={listItems} />
         </div>
     );
 }
---
+++ src/components/YTSidePanel/items.js
@@ -0,0 +1,14 @@
+const listItems = [
+    {
+        id: 1,
+        name: "Home",
+        icon: <i class="fa-solid fa-house"></i>
+    },
+    {
+        id: 2,
+        name: "About",
+        icon: <i class="fa-solid fa-address-card"></i>
+    }
+];
+
+export default listItems;
