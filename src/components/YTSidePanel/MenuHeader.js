7cc713287bd7f24530dc3c811c17e882d7366271
jameydev <jybryce4@gmail.com>
2023-05-16 08:20:49 -0500

    started work on youtube side panel components

---
+++ src/components/YTSidePanel/MenuHeader.js
@@ -0,0 +1,10 @@
+import HamburgerIcon from "./HamburgerIcon";
+
+export default function MenuHeader() {
+    return (
+        <div class="menu-header">
+            <HamburgerIcon />
+            <img className="logo" src="#" alt="YOUR LOGO/ICON" />
+        </div>
+    );
+}
---
+++ src/components/YTSidePanel/SideMenu.js
@@ -0,0 +1,9 @@
+import MenuHeader from "./MenuHeader";
+
+export default function SideMenu() {
+    return (
+        <div className="side-menu-wrapper">
+            <MenuHeader />
+        </div>
+    );
+}
